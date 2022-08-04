c4cc0e07d44070e299fe2795eb4e87c5dfaa5c43
tangzheng <tangzheng0105@gmail.com>
2022-08-04 14:12:34 +0800

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
 import '../styles/globals.css'
+import '../styles/css/bootstrap.css'
+import '../styles/css/bxslider.css'
+import '../styles/css/lightbox.css'
+import '../styles/css/online.css'
+import '../styles/css/style.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
